4f2ddb29cf083bb1b3bcdbbd86c408b4002ca879
Donggil Wee <coalla7303@gmail.com>
2025-06-04 10:31:15 +0900

    1234

--- basic_syntax/basic_commands.sh
+++ basic_syntax/basic_commands.sh
@@ -10,3 +10,11 @@ git add testfolder/test1.txt
 git commit -m "메시지제목" -m "메시지내용"
 # git commit만 입력하고 엔터시 vi모드 -> 첫줄:타이틀, 두번째줄부터 내용
 git commit
+
+# commit이력 확인
+git log --oneline
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+# 충돌발생시 충돌무시하고, 로컬기준으로 원격에 덮어쓰기
+git push origin 브랜치명 --force
